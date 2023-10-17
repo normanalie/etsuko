a2a3ef7452791dc313d4d7ddd4c228630e54f4c0
Norman ALIE <mail@normanalie.fr>
2023-10-17 09:33:03 +0200

    Export config data

---
+++ config/channels.js
@@ -0,0 +1,10 @@
+const channels = {
+    choix_de_la_filliere: '884497820743237692',
+    changement_filliere: '884834854615785522',
+    roles_detente: '884842022589050900',
+    vision_d_aigle: '886387568093065226',
+    choix_lv2_cpi2: '884498054466666587',
+    choix_lv2_iatic: '884498154794414080',
+}
+
+module.exports = channels
