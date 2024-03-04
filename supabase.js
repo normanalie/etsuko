141493fe23c9c4c47e1af93cafa0ba210bd3c8ae
Norman Alié <mail@normanalie.fr>
2024-03-04 11:20:50 +0100

    Dev database

--- supabase.js
+++ supabase.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const { createClient } = require('@supabase/supabase-js')
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-    'https://xwnpgapjafoyukdfeunm.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3bnBnYXBqYWZveXVrZGZldW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzOTMwMDEsImV4cCI6MjAxMzk2OTAwMX0.91pRE9tyY3R0NsaFtrQF3AN7MJX_wtet-cgpgT_j8Rc'
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_PUBLIC_KEY
 )
 
 exports.supabase = supabase
