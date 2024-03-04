4b54d23c789091007d119c5dd98ddb6e933c023f
Norman Alié <mail@normanalie.fr>
2024-03-04 11:39:09 +0100

    jsdoc

--- misc/inThread.js
+++ misc/inThread.js
@@ -1,5 +1,11 @@
 const { ChannelType } = require('discord.js')
 
+/**
+ * Check if the interaction is in a thread.
+ *
+ * @param {type} interaction - the interaction to check
+ * @return {boolean} true if the interaction is in a thread, false otherwise
+ */
 const inThread = (interaction) => {
     chanType = interaction.channel.type
     if (
