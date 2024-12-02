310a104b0c239793ba604d864cb563e814c69127
Constance <150241836+ConstanceALOYAU@users.noreply.github.com>
2024-12-02 18:02:05 -0500

    Create AoC.js

---
+++ commands/AoC.js
@@ -0,0 +1,138 @@
+	
+event	"2024"
+members	
+1033978	
+last_star_ts	1733120273
+global_score	0
+name	"Mr-Robot-echo-hello-world"
+completion_day_level	
+1	
+1	
+get_star_ts	1733034479
+star_index	0
+2	
+get_star_ts	1733034679
+star_index	347
+2	
+1	
+get_star_ts	1733119788
+star_index	224727
+2	
+get_star_ts	1733120273
+star_index	226667
+id	1033978
+stars	4
+local_score	28
+3454311	
+completion_day_level	
+1	
+1	
+get_star_ts	1733172185
+star_index	438409
+2	
+star_index	441028
+get_star_ts	1733172833
+2	
+1	
+star_index	446538
+get_star_ts	1733174188
+last_star_ts	1733174188
+global_score	0
+name	"Constance"
+stars	3
+local_score	8
+id	3454311
+4353313	
+completion_day_level	
+1	
+1	
+star_index	94266
+get_star_ts	1733068636
+2	
+star_index	168997
+get_star_ts	1733093344
+2	
+1	
+star_index	353064
+get_star_ts	1733151304
+2	
+get_star_ts	1733156500
+star_index	374729
+global_score	0
+last_star_ts	1733156500
+name	"Floris"
+local_score	21
+stars	4
+id	4353313
+4385319	
+id	4385319
+stars	3
+local_score	18
+global_score	0
+last_star_ts	1733150525
+name	"MelBlo"
+completion_day_level	
+1	
+1	
+get_star_ts	1733067545
+star_index	91193
+2	
+get_star_ts	1733068223
+star_index	93094
+2	
+1	
+star_index	349756
+get_star_ts	1733150525
+4392588	
+id	4392588
+local_score	0
+stars	0
+last_star_ts	0
+global_score	0
+name	null
+completion_day_level	{}
+4443909	
+last_star_ts	1733168424
+name	"Alexis Fiolet"
+global_score	0
+completion_day_level	
+1	
+1	
+star_index	184474
+get_star_ts	1733103880
+2	
+get_star_ts	1733104154
+star_index	184753
+2	
+1	
+star_index	412769
+get_star_ts	1733165892
+2	
+get_star_ts	1733168424
+star_index	422880
+id	4443909
+stars	4
+local_score	17
+4527158	
+id	4527158
+local_score	11
+stars	4
+last_star_ts	1733176515
+name	"Plus Utomega"
+global_score	0
+completion_day_level	
+1	
+1	
+star_index	454270
+get_star_ts	1733176224
+2	
+star_index	455339
+get_star_ts	1733176515
+2	
+1	
+get_star_ts	1733167666
+star_index	419821
+2	
+get_star_ts	1733174948
+star_index	449532
+owner_id	1033978
